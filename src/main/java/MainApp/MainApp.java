0c848b500f6fdc3b34198aab7d4a16c299ce60bf
Adisalam <adisalamramdani1290@gmail.com>
2026-05-03 16:52:52 +0700

    FirstCommit

---
+++ src/main/java/MainApp/MainApp.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package MainApp;
+
+import Object.Guru;
+
+/**
+ *
+ * @author ACER
+ */
+public class MainApp {
+
+    public static void main(String[] args) {
+        Guru KR = new Guru();
+        if (KR instanceof Guru) {
+            System.err.println("Guru");
+        } else {
+            System.err.println("Something else");
+        }// //
+        // //
+    }
+}
---
+++ src/main/java/Object/Guru.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Object;
+
+/**
+ *
+ * @author ACER
+ */
+public class Guru {
+    private String uidRfid;
+    private String idKaryawan;
+    private String namaLengkap;
+    private String departemen;
+    
+    public Guru() {
+    }
+    
+    public Guru(String uidRfid, String idKaryawan, String namaLengkap, String departemen) {
+        this.uidRfid = uidRfid;
+        this.idKaryawan = idKaryawan;
+        this.namaLengkap = namaLengkap;
+        this.departemen = departemen;
+    }
+
+    public String getUidRfid() {
+        return uidRfid;
+    }
+
+    public void setUidRfid(String uidRfid) {
+        this.uidRfid = uidRfid;
+    }
+
+    public String getIdKaryawan() {
+        return idKaryawan;
+    }
+
+    public void setIdKaryawan(String idKaryawan) {
+        this.idKaryawan = idKaryawan;
+    }
+
+    public String getNamaLengkap() {
+        return namaLengkap;
+    }
+
+    public void setNamaLengkap(String namaLengkap) {
+        this.namaLengkap = namaLengkap;
+    }
+
+    public String getDepartemen() {
+        return departemen;
+    }
+
+    public void setDepartemen(String departemen) {
+        this.departemen = departemen;
+    }
+    
+    
+}
